94eaf2d3fdbe427139554e10a12fd306bdde1150
Anthony Mazzawi <anthonymazzawi@gmail.com>
2017-06-02 12:05:51 +0100

    Function to extract specific time data from mongo into pandas

--- stat_dataframe.py
+++ stat_dataframe.py
@@ -6,41 +6,82 @@ import pandas as pd
 stat_names = ['orb', 'drb', 'fgm', 'fga', 'assist', 'points', 'ftm', 'fta',
               'fg3a', 'fg3m', 'turnover', 'foul', 'timeout', 'sub']
 
+teams = ['ATL', 'BOS', 'BRK', 'CHO', 'CHI', 'CLE', 'DAL', 'DEN', 'DET', 'GSW', 'HOU', 'IND', 'LAC', 'LAL', 'MEM', 'MIA',
+         'MIL', 'MIN', 'NOP', 'NYK', 'OKC', 'ORL', 'PHI', 'PHO', 'POR', 'SAC', 'SAS', 'TOR', 'UTA', 'WAS']
 
-class stat_dataframe:
-    def __init__(self, stat=None):
 
-        if stat is None:
-            stat = ['points']
+def get_times(season=None, team=None, home=None, home_win=None):
+    time_data = {}
 
-        for key in stat:
-            if key not in stat_names:
-                raise ValueError('Incorrect Statistic Key')
+    # Check season values
+    if not isinstance(season, list):
+        if season is not None:
+            raise TypeError("Season must be a list for query purposes")
+        else:
+            season = [2012, 2013, 2014, 2015, 2016, 2017]
 
-        self.stat = stat
+    else:
+        for year in season:
+            if year < 2012 or year > 2017:
+                raise ValueError("Years must be within the range 2012-2017")
 
-        print('Processing %s time data...' % self.stat)
+    # Check team values
+    if not isinstance(team, list):
+        if team is not None:
+            raise TypeError("Team must be a list for query purposes")
+        else:
+            team = teams
+    else:
+        for city in team:
+            if city not in teams:
+                raise ValueError("Team not in database")
 
-        self.data = self.get_times()
+    # Check Home Value
+    if not isinstance(home, bool):
+        if home is not None:
+            raise TypeError("Home must be a Boolean value or None")
 
-    def get_times(self):
+    # Check Home Win Value
+    if not isinstance(home_win, bool):
+        if home_win is not None:
+            raise TypeError("Home Win must be a Boolean value or None")
 
-        time_data = {}
-        client = MongoClient()
-        db = client.basketball
-        collection = db.game_log
+    if home_win is None:
+        result = [1, -1]
+    elif home_win is True:
+        result = [1]
+    else:
+        result = [-1]
 
+    # MongoDB
+    client = MongoClient()
+    db = client.basketball
+    collection = db.game_log
+
+    if home is None:
         locations = ['home_time', 'away_time']
+        games = collection.find(
+            {'result': {'$in': result}, 'season': {'$in': season},
+             '$or': [{'home.team': {'$in': team}}, {'away.team': {'$in': team}}]},
+            {'home_time': 1, 'away_time': 1})
+    elif home is True:
+        locations = ['home_time']
+        games = collection.find({'result': {'$in': result}, 'season': {'$in': season}, 'home.team': {'$in': team}},
+                                {'home_time': 1})
+    else:
+        locations = ['away_time']
+        games = collection.find({'result': {'$in': result}, 'season': {'$in': season}, 'away.team': {'$in': team}},
+                                {'away_time': 1})
 
+    for game in games:
         for loc in locations:
-            games = collection.find({}, {loc: 1})
+            for stat in game[loc]:
 
-            for game in games:
-                for stat in game[loc]:
+                time = int(stat['time']) + 1
 
-                    time = int(stat['time']) + 1
-
-                    for key in self.stat:
+                # Only want regulation time
+                if time <= 48:
+                    for key in ['points']:
                         if key in stat:
 
                             if time not in time_data:
@@ -52,20 +93,7 @@ class stat_dataframe:
                             else:
                                 time_data[time][key] = time_data[time][key] + stat[key]
 
-        data = pd.DataFrame(time_data)
-        data = data.transpose()
+    data = pd.DataFrame(time_data)
+    data = data.transpose()
 
-        return data
-
-matplotlib.style.use('ggplot')
-
-stats = stat_dataframe()
-
-ax = stats.data.plot(title='Points' + ' Minute Distribution for Last 6 years of NBA')
-ax.set_xlabel('Minute')
-ax.set_ylabel('Points')
-ax.axvline(48, linestyle='dashed', color='black', linewidth=1)
-ax.axvline(12, linestyle='dashed', color='black', linewidth=0.5)
-ax.axvline(24, linestyle='dashed', color='black', linewidth=0.5)
-ax.axvline(36, linestyle='dashed', color='black', linewidth=0.5)
-plt.show()
+    return data
