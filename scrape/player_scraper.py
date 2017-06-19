de1a9d701bc5a3caefd5ca182a669c80d33db37f
Anthony Mazzawi <anthonymazzawi@gmail.com>
2017-06-19 12:29:27 +0100

    Split up scrape files

---
+++ scrape/player_scraper.py
@@ -0,0 +1,138 @@
+import string
+
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_starting_lineups(team, year):
+    """
+    Scrape a team's starting lineup for every game in a season.
+
+    :param team: NBA Team (Team abbreviation)
+    :param year: NBA Season
+    :return: Dict containing date and starting lineup
+    """
+
+    # Starting Lineup URL
+    url = "http://www.basketball-reference.com/teams/%s/%s_start.html" % (team, year)
+
+    r = requests.get(url)
+    r.raise_for_status()
+
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    # Line up table
+    lineup_table = soup.find(id='starting_lineups').find('tbody')
+
+    lineups = []
+
+    # Iterate through each game
+    for game in lineup_table.find_all('tr', {'class': None}):
+
+        # TODO: Convert Date datetime to be able to query mongodb
+        date = game.find('td', {'data-stat': 'date_game'}).text
+
+        lineup = {
+            'date': date,
+            'starters': []
+        }
+
+        # Get the starting lineup
+        starters = game.find('td', {'data-stat': 'game_starters'})
+        for player in starters.find_all('a'):
+            lineup['starters'].append(player.text)
+
+        lineups.append(lineup)
+
+    return lineups
+
+
+def get_active_players():
+    """ Get a list of all active NBA players (name and url to stats page) """
+
+    active_players = []
+
+    # Iterate through each letter of the alphabet
+    for letter in string.ascii_lowercase:
+        url = "http://www.basketball-reference.com/players/%s/" % letter
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, "html.parser")
+
+        # Not every letter is represented by a player
+        try:
+
+            # Player table
+            player_table = soup.find(id='players').find('tbody')
+
+            # Iterate through each player, active players have a <strong> tag
+            for player in player_table.find_all('tr'):
+
+                active = player.find('strong')
+
+                if active is not None:
+                    player_info = {
+                        'name': active.text,
+                        'url': active.find('a')['href']
+                    }
+
+                    active_players.append(player_info)
+
+        except Exception as e:
+            print(e)
+
+    return active_players
+
+
+def get_player_stats(player):
+    """ Scrape a player's yearly stats"""
+
+    # TODO: Add a player's advanced stats, right now it is only per game
+
+    url = "http://www.basketball-reference.com" + player['url']
+
+    # Request
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    # Player's statistics
+    per_game = soup.find(id="per_game").find('tbody')
+
+    # When there are missing years, there is no id or data-stat for the year
+    regex = re.compile('.*')
+
+    # Player dictionary
+    player_stats = {
+        '_id': url.rsplit('/', 1)[-1].rsplit('.', 1)[0],
+        'name': player['name']
+    }
+
+    # These entries are defined in the per game and advanced tables
+    # Only want them to be displayed once per season for a player
+    entries = ['age', 'team_id', 'lg_id', 'pos', 'g', 'gs', 'mp']
+
+    # Iterate through the years
+    for year in per_game.find_all('tr', {'id': regex}):
+
+        # Season stats
+        season = {}
+
+        season_year = year['id'][9:13]
+
+        # If a player is traded midway through the season, the year's totals for
+        # both teams is the first row.  Right now only the totals are stored
+        if season_year not in player_stats:
+            player_stats[season_year] = {}
+
+            # Each stat in a season (Per Game)
+            for stat in year.find_all('td', {'data-stat': regex}):
+                season[stat['data-stat']] = stat.string
+
+            for key in entries:
+                if key in season:
+                    player_stats[season_year][key] = season[key]
+                    del season[key]
+
+            player_stats[season_year]['per_g'] = season
+
+    return player_stats
---
+++ scrape/scrape_utils.py
@@ -0,0 +1,212 @@
+from datetime import datetime
+
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+def team_names():
+    """
+    Scrape all team names of the NBA
+
+    :return: list of NBA Teams
+    """
+
+    # Url for all teams in the NBA
+    url = 'http://www.basketball-reference.com/teams/'
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    # Page also lists defunct franchises, only want currently active teams
+    active_teams = soup.find(id="teams_active")
+
+    teams = []
+
+    # Iterate through teams to get the team abbreviation (ex. TOR)
+    for active_team in active_teams.find_all('tr', {'class': "full_table"}):
+        team_url = active_team.find('a')
+        team_name = team_url['href']
+        teams.append(team_name[7:10])
+
+    return teams
+
+
+def rename_team(team, year=None):
+    """
+    Rename a team that has relocated to keep the database consistent
+
+    :param year: NBA Season
+    :param team: Team Abbreviation to be named
+    :return: Changed team name if the team has relocated, otherwise the same name is returned
+
+    """
+
+    # Rename relocated team to current abbreviation
+    if team == 'NJN':
+        if year is None or year > 2012:
+            team = 'BRK'
+    elif team == 'CHA':
+        if year is None or year > 2014:
+            team = 'CHO'
+    elif team == 'NOH':
+        if year is None or year > 2013:
+            team = 'NOP'
+
+    return team
+
+
+def determine_home_win(location, result):
+    """
+    Determine the result of the home team given the location and result for a a specific team
+
+    :param location: Location of the game (None for Home, @ for Away)
+    :param result: Result of the game (W for Win, L for Loss)
+    :return: 1 or -1 for the home result
+    :raises Value Error: If result is not W or L, and if location is not None or @
+
+    """
+
+    if result != 'W' and result != 'L':
+        raise ValueError('The game result is incorrect, must be W or L')
+
+    if location is not None and location != '@':
+        raise ValueError('Location is incorrectly entered')
+
+    # Determine Home Winner
+    if location is None:
+        if result == 'W':
+            return 1
+        else:
+            return -1
+    else:
+        if result == 'L':
+            return 1
+        else:
+            return -1
+
+
+def stat_distribution(url):
+    # Request
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    # Play by play table
+    table = soup.find(id='pbp').find_all('tr')
+
+    stat_dist = {
+        'home': [],
+        'away': [],
+    }
+
+    quarter = 0
+    for item in table:
+        time = None
+
+        x = 0
+
+        pattern = re.compile('^[0-9]{1,3}:[0-9]{2}\.[0-9]{1}$')
+
+        score = {}
+        for stat in item.find_all('td'):
+
+            x += 1
+
+            check = True
+
+            if "makes" in stat.text:
+
+                player = (stat.find('a')['href'])
+                player = player.rsplit('/', 1)[-1].rsplit('.', 1)[0]
+                score['player'] = player
+
+                if 'assist' in stat.text:
+                    score['assist'] = 1
+
+                if '3-pt' in stat.text:
+                    score['points'] = 3
+                    score['fgm'] = 1
+                    score['fg3m'] = 1
+                    score['fga'] = 1
+                elif '2-pt' in stat.text:
+                    score['points'] = 2
+                    score['fgm'] = 1
+                    score['fga'] = 1
+                elif 'free' in stat.text:
+                    score['points'] = 1
+                    score['ftm'] = 1
+                    score['fta'] = 1
+            elif "misses" in stat.text:
+
+                player = (stat.find('a')['href'])
+                player = player.rsplit('/', 1)[-1].rsplit('.', 1)[0]
+                score['player'] = player
+
+                if '3-pt' in stat.text:
+                    score['fga'] = 1
+                    score['fg3a'] = 1
+                elif '2-pt' in stat.text:
+                    score['fga'] = 1
+                elif 'free' in stat.text:
+                    score['fta'] = 1
+            elif "Defensive rebound" in stat.text:
+                if 'Team' not in stat.text:
+                    score['drb'] = 1
+            elif "Offensive rebound" in stat.text:
+                if 'Team' not in stat.text:
+                    score['orb'] = 1
+            elif "Turnover" in stat.text:
+                score['turnover'] = 1
+            elif "foul" in stat.text:
+                score['foul'] = 1
+            elif "timeout" in stat.text:
+                score['timeout'] = 1
+            elif "enters" in stat.text:
+                score['sub'] = 1
+            else:
+                check = False
+
+            if score:
+
+                if check is True:
+                    if x == 2:
+                        score['home'] = 0
+                    elif x == 6:
+                        score['home'] = 1
+
+            if pattern.match(stat.text):
+                if quarter == 2:
+                    date = datetime.strptime("12:00", "%M:%S")
+                elif quarter == 4:
+                    date = datetime.strptime("24:00", "%M:%S")
+                elif quarter == 6:
+                    date = datetime.strptime("36:00", "%M:%S")
+                elif quarter == 8:
+                    date = datetime.strptime("48:00", "%M:%S")
+                elif quarter == 10:
+                    date = datetime.strptime("53:00", "%M:%S")
+                elif quarter == 12:
+                    date = datetime.strptime("58:00", "%M:%S")
+                else:
+                    date = datetime.strptime("1:03:00", "%H:%M:%S")
+
+                time = datetime.strptime(stat.text[:-2], "%M:%S")
+                time = date - time
+                time = divmod(time.days * 86400 + time.seconds, 60)
+                time = time[0] + time[1] / 60
+                time = round(time, 2)
+
+        if score:
+            score['time'] = time
+
+            if score['home'] == 1:
+                del score['home']
+                stat_dist['home'].append(score)
+            else:
+                del score['home']
+                stat_dist['away'].append(score)
+
+        if time is None:
+            quarter += 1
+
+    return stat_dist
