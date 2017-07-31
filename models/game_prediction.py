9ea77c63fce5b43ed927339292d63d7430ec0944
Anthony Mazzawi <anthonymazzawi@gmail.com>
2017-07-31 11:59:02 +0100

    Game probabilities time improved

--- models/game_prediction.py
+++ models/game_prediction.py
@@ -12,22 +12,23 @@ def determine_probabilities(hmean, amean):
     :return: Probabilities of home and away team
     """
 
-    hprob, aprob = 0, 0
+    # Possible scores
+    scores = np.arange(0, 200)
+
+    hprob = np.zeros(len(scores))
+    aprob = np.zeros(len(scores))
 
     # The probability for the home team is the sum of the poisson probabilities when h > a and
     # vice versa for the away team
-    for h in range(60, 140):
-        for a in range(60, 140):
+    for x in scores:
+        hprob[x] = np.sum(poisson.pmf(mu=hmean, k=x) * poisson.pmf(mu=amean, k=np.where(scores < x)))
+        aprob[x] = np.sum(poisson.pmf(mu=amean, k=x) * poisson.pmf(mu=hmean, k=np.where(scores < x)))
 
-            if h > a:
-                hprob += (poisson.pmf(mu=hmean, k=h) * poisson.pmf(mu=amean, k=a))
-            elif h < a:
-                aprob += (poisson.pmf(mu=hmean, k=h) * poisson.pmf(mu=amean, k=a))
-
-    return hprob, aprob
+    # Return sum
+    return np.sum(hprob), np.sum(aprob)
 
 
-def dixon_prediction(season, abilities=None, display=False):
+def dixon_prediction(season, abilities=None):
     """
     Dixon Coles or Robinson game prediction based off the team probabilities.
 
@@ -40,12 +41,15 @@ def dixon_prediction(season, abilities=None, display=False):
     mongo = mongo_utils.MongoDB()
 
     # Testing Dataset
-    test = datasets.dc_dataframe(season=season)
+    test = datasets.dc_dataframe(season=season, bet=True)
 
-    ngames = np.zeros(100)
-    ncorrect = np.zeros(100)
+    # ngames = np.zeros(100)
+    # ncorrect = np.zeros(100)
 
     date = None
+
+    hprob = np.zeros(len(test))
+    aprob = np.zeros(len(test))
 
     # Iterate through each game to determine the winner and prediction
     for row in test.itertuples():
@@ -58,30 +62,20 @@ def dixon_prediction(season, abilities=None, display=False):
         hmean = abilities[row.home]['att'] * abilities[row.away]['def'] * abilities['home']
         amean = abilities[row.away]['att'] * abilities[row.home]['def']
 
-        hprob, aprob = determine_probabilities(hmean, amean)
+        hprob[row.Index], aprob[row.Index] = determine_probabilities(hmean, amean)
 
-        if row.hpts > row.apts:
-            winner = row.home
-        else:
-            winner = row.away
+    test['hprob'] = hprob
+    test['aprob'] = aprob
 
-        if hprob > aprob:
-            predict = row.home
-            predict_prob = hprob
-        else:
-            predict = row.away
-            predict_prob = aprob
+    # Actual winners and predictions
+    winners = np.where(test.hpts > test.apts, test.home, test.away)
+    predictions = np.where(test.hprob > test.aprob, test.home, test.away)
 
-        if winner == predict:
-            correct = True
-        else:
-            correct = False
+    # Determine correct predictions
+    correct = np.equal(winners, predictions)
 
-        if correct:
-            ncorrect[int(predict_prob * 100)] += 1
-        ngames[int(predict_prob * 100)] += 1
+    # if correct:
+    #    ncorrect[int(predict_prob * 100)] += 1
+    # ngames[int(predict_prob * 100)] += 1
 
-        if display:
-            print('Game: %d\t\tPredicted: %s\t\tWinner: %s' % (row.Index, predict, winner))
-
-    return ncorrect, ngames
+    return sum(correct) / len(winners)
